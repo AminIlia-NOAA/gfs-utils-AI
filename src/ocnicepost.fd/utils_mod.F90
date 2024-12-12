5254b306ec74ff1c8269eaae0dd25638d0a2b186
Amin Ilia <1621812366113305@mil>
2024-12-12 10:42:55 -0500

    changed parameters for section 5

--- src/ocnicepost.fd/utils_mod.F90
+++ src/ocnicepost.fd/utils_mod.F90
@@ -750,7 +750,7 @@ contains
          ! Populate idrtmpl for Template 5.0
          idrtmpl(1) = 0             ! Reference value (scaled value of the minimum data point)
          idrtmpl(2) = 0             ! Binary scale factor (scale by 2^E)
-         idrtmpl(3) = 2             ! Decimal scale factor (scale by 10^D)
+         idrtmpl(3) = 3             ! Decimal scale factor (scale by 10^D)
          idrtmpl(4) = 0             ! Number of bits for each packed value
          idrtmpl(5) = 0             ! Type of original field values (0 = floating point)
          ! Reserved fields for Template 5.0
